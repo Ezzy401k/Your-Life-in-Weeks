9bc0afa997434e316752c87f0ac264abdb7f47d1
Esrael Mekdem <esraelmekdem@gmail.com>
2024-02-26 14:39:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 age = input("What is your current age?\n")
 # we then convert the age to intigers
 age = int(age)
+# System guesses at random how long a user lives
+pridicted_age = random.randint(age,110)
 # we calculate the years one has left
-year_left = 90 - age
+year_left = pridicted_age - age
 # the months one has left
 months_left = (year_left * 12)
 # weeks one has left
@@ -11,4 +13,5 @@ weeks_left = (year_left * 52)
 # days one has left
 days_left = (year_left * 365)
 # we use f-string to print the output
-print(f"You have {days_left} left,{weeks_left} left, and {months_left} months left")
+print(f"You have {days_left} days,{weeks_left} weeks, {months_left} months and {year_left} years left.")
+input("press enter to EXIT!")
