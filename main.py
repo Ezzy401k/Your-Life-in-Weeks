e4da0d1920da478c8c55be7f07da48d3336de568
Esrael Mekdem <esraelmekdem@gmail.com>
2024-02-27 09:51:36 +0300

    Update main.py
    added a random decider of the max year a user cal live.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import random
+
 #First we input the age
 age = input("What is your current age?\n")
 # we then convert the age to intigers
 age = int(age)
-# System guesses at random how long a user lives
-pridicted_age = random.randint(age,110)
+last_year = int(random.random() * 100)
+#System guesses at random how long a user lives
+pridicted_age = random.randint(age,last_year)
 # we calculate the years one has left
 year_left = pridicted_age - age
 # the months one has left
@@ -13,5 +16,6 @@ weeks_left = (year_left * 52)
 # days one has left
 days_left = (year_left * 365)
 # we use f-string to print the output
-print(f"You have {days_left} days,{weeks_left} weeks, {months_left} months and {year_left} years left.")
+print(f"You have {days_left} in days,{weeks_left} in weeks, {months_left} in months and {year_left} in years left.")
 input("press enter to EXIT!")
+# print(type(last_year))
